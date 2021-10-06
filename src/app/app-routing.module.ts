e8fd69607b2ba08bfb006f74bde22101df20507c
Maabia <maabia.ferry16@gmail.com>
2021-10-06 19:05:12 -0300

    rota vídeos criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoListComponent } from './features/video/video-list/video-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'videos'
+  },
+  {
+    path: "videos",
+    component: VideoListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
