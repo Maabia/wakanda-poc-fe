46c725dc0aa60a07287d6d9a2d44b16b0d32b5e3
Maabia <maabia.ferry16@gmail.com>
2021-10-11 22:10:29 -0300

    service refatorado

--- src/app/features/video/service/videos.service.ts
+++ src/app/features/video/service/videos.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Videos } from '../interfaces/videos';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+
 
 
 @Injectable({
