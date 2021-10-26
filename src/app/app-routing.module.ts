b997e1d4de9acc3476aee90a3500654c0d28bd16
Maabia <maabia.ferry16@gmail.com>
2021-10-26 16:44:12 -0300

    refatoração da rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { VideoListComponent } from './features/video/video-list/video-list.component';
+
 
 const routes: Routes = [
   {
@@ -9,8 +9,8 @@ const routes: Routes = [
     redirectTo: 'video'
   },
   {
-    path: 'video/:id',
-    component: VideoListComponent
+    path: 'video',
+    loadChildren: () => import('./features/video/video.module').then(m => m.VideoModule)
   },
 
 ];
--- src/app/features/video/video-routing.module.ts
+++ src/app/features/video/video-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoListComponent } from './video-list/video-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'video/:idVideo',
+    component: VideoListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
