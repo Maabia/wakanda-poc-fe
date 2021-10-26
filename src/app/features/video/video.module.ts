95c39e0c7fc8abd3be58942879797cefc3fd9ae7
Maabia <maabia.ferry16@gmail.com>
2021-10-26 11:36:24 -0300

    modulo

--- src/app/features/video/video.module.ts
+++ src/app/features/video/video.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VideoRoutingModule } from './video-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    VideoRoutingModule
+    VideoRoutingModule,
+    RouterModule
   ]
 })
 export class VideoModule { }
